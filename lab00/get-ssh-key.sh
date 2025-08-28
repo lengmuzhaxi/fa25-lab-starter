cb9c807460e92f9bc928ce76d7b68d29863bafee
Andrea Lou <andrea.lou@berkeley.edu>
2025-08-27 23:09:16 -0700

    fix(lab00): get ssh key

--- lab00/get-ssh-key.sh
+++ lab00/get-ssh-key.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 
-KEY_FILE="$HOME/.ssh/61c-ed25519"
+SSH_HOST=${1:-s275-4.cs.berkeley.edu}
 
-if [ ! -f "$KEY_FILE" ]; then
-    ssh-keygen -t ed25519 -f "$KEY_FILE" -C "61Ced25519Key" -N ""
-    echo "Great! Your new SSH key is at: $KEY_FILE" 
-else
-    echo "SSH key already exists at $KEY_FILE. Skipping generation."
-fi
+for OUTPUT in $(ssh "$SSH_HOST" -G | grep "^identityfile " | cut -d " " -f2-)
+do
+    filename="${OUTPUT/#\~/$HOME}"
+    if [[ -f $filename ]]; then
+        echo "You already have an SSH key at: $OUTPUT"
+        echo "Make note of this, as you'll need this information later!"
+        exit 0
+    fi
+done
+
+echo "I couldn't find an existing SSH key for you, so let's make one now..."
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ''
+echo "Great! Your new SSH key is at: ~/.ssh/id_ed25519"
+echo "Make note of this, as you'll need this information later!"
